c39b42a408e1e2575142423b726db24df948aebc
vidhisharma1212 <85745770+vidhisharma1212@users.noreply.github.com>
2024-10-24 23:44:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+}
+
+function goTo(page) {
+    // Navigation logic can be added here
+    alert(`Navigating to ${page}`);
+}
